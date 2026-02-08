8db222d8d643c80f9db0c8ff772d85a35b9f1bc0
Nandkumar Tekale <tekalenandkumar@gmail.com>
2026-02-08 15:43:13 +0530

    feat: rename application class and simplify Maven Resolver configuration with `RepositorySystemSupplier`

--- backend/src/main/java/com/modernmvn/backend/config/MavenConfig.java
+++ backend/src/main/java/com/modernmvn/backend/config/MavenConfig.java
@@ -1,13 +1,7 @@
 package com.modernmvn.backend.config;
 
-import org.apache.maven.repository.internal.MavenRepositorySystemUtils;
 import org.eclipse.aether.RepositorySystem;
-import org.eclipse.aether.connector.basic.BasicRepositoryConnectorFactory;
-import org.eclipse.aether.impl.DefaultServiceLocator;
-import org.eclipse.aether.spi.connector.RepositoryConnectorFactory;
-import org.eclipse.aether.spi.connector.transport.TransporterFactory;
-import org.eclipse.aether.transport.file.FileTransporterFactory;
-import org.eclipse.aether.transport.http.HttpTransporterFactory;
+import org.eclipse.aether.supplier.RepositorySystemSupplier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -16,18 +10,6 @@ public class MavenConfig {
 
     @Bean
     public RepositorySystem repositorySystem() {
-        DefaultServiceLocator locator = MavenRepositorySystemUtils.newServiceLocator();
-        locator.addService(RepositoryConnectorFactory.class, BasicRepositoryConnectorFactory.class);
-        locator.addService(TransporterFactory.class, FileTransporterFactory.class);
-        locator.addService(TransporterFactory.class, HttpTransporterFactory.class);
-
-        locator.setErrorHandler(new DefaultServiceLocator.ErrorHandler() {
-            @Override
-            public void serviceCreationFailed(Class<?> type, Class<?> impl, Throwable exception) {
-                exception.printStackTrace();
-            }
-        });
-
-        return locator.getService(RepositorySystem.class);
+        return new RepositorySystemSupplier().get();
     }
 }
